2bc6f55d5b4d1c9d0a0d46479df181fc745b1f44
MillaDom <milladom@gmail.com>
2023-01-23 17:20:25 +0200

    fixes #2, #9

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -37,10 +37,11 @@ public class Player {
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
-            playedTime.put(game, playedTime.get(game));
+            playedTime.put(game, playedTime.get(game) + hours);
         } else {
-            playedTime.put(game, hours);
+            throw new RuntimeException("У игрока " + this.name + " игра " + game + " не установлена!");
         }
+        game.getStore().addPlayTime(name, hours);
         return playedTime.get(game);
     }
 
@@ -51,8 +52,7 @@ public class Player {
         for (Game game : playedTime.keySet()) {
             if (game.getGenre().equals(genre)) {
                 sum += playedTime.get(game);
-            } else {
-                sum = 0;
+
             }
         }
         return sum;
