0b76740bc9f4bbdb7a72f80c8a0554d2a7dde68c
MillaDom <milladom@gmail.com>
2023-01-24 22:40:15 +0200

    Player corrected

--- src/main/java/ru/netology/Player.java
+++ src/main/java/ru/netology/Player.java
@@ -6,9 +6,11 @@ import java.util.Map;
 public class Player {
     private String name;
 
-    /** информация о том, в какую игру сколько часов было сыграно
-    ключ - игра
-    значение - суммарное количество часов игры в эту игру */
+    /**
+     * информация о том, в какую игру сколько часов было сыграно
+     * ключ - игра
+     * значение - суммарное количество часов игры в эту игру
+     */
     private Map<Game, Integer> playedTime = new HashMap<>();
 
     public Map<Game, Integer> getPlayedTime() {
@@ -23,19 +25,23 @@ public class Player {
         return name;
     }
 
-    /** добавление игры игроку
-    если игра уже была, никаких изменений происходить не должно */
+    /**
+     * добавление игры игроку
+     * если игра уже была, никаких изменений происходить не должно
+     */
     public void installGame(Game game) {
         if (!playedTime.containsKey(game)) {
             playedTime.put(game, 0);
         }
     }
 
-    /** игрок играет в игру game на протяжении hours часов
-    об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
-    также надо обновить значения в мапе игрока, добавив проигранное количество часов
-    возвращает суммарное количество часов, проигранное в эту игру.
-    если игра не была установлена, то надо выкидывать RuntimeException */
+    /**
+     * игрок играет в игру game на протяжении hours часов
+     * об этом нужно сообщить объекту-каталогу игр, откуда была установлена игра
+     * также надо обновить значения в мапе игрока, добавив проигранное количество часов
+     * возвращает суммарное количество часов, проигранное в эту игру.
+     * если игра не была установлена, то надо выкидывать RuntimeException
+     */
     public int play(Game game, int hours) {
         game.getStore().addPlayTime(name, hours);
         if (playedTime.containsKey(game)) {
@@ -47,8 +53,10 @@ public class Player {
         return playedTime.get(game);
     }
 
-    /** Метод принимает жанр игры (одно из полей объекта игры) и
-     суммирует время, проигранное во все игры этого жанра этим игроком */
+    /**
+     * Метод принимает жанр игры (одно из полей объекта игры) и
+     * суммирует время, проигранное во все игры этого жанра этим игроком
+     */
     public int sumGenre(String genre) {
         int sum = 0;
         for (Game game : playedTime.keySet()) {
@@ -60,20 +68,23 @@ public class Player {
         return sum;
     }
 
-    /** Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
-     Если в игры этого жанра не играли, возвращается null */
+    /**
+     * Метод принимает жанр и возвращает игру этого жанра, в которую играли больше всего
+     * Если в игры этого жанра не играли, возвращается null
+     */
     public Game mostPlayerByGenre(String genre) {
         int maxTime = 0;
+        Game maxGame = null;
         for (Game game : playedTime.keySet()) {
-            if (game.getGenre().equals(genre) && playedTime.get(game) > maxTime) {
-                maxTime = playedTime.get(game);
+            if (game.getGenre().contains(genre)) {
+                if (playedTime.get(game) >= maxTime) {
+                    maxGame = game;
+                    maxTime = playedTime.get(game);
+                }
             }
         }
-        for (Game game : playedTime.keySet()) {
-            if (maxTime == playedTime.get(game)) {
-                return game;
-            }
-        }
-        return null;
+        return maxGame;
     }
+
+
 }
