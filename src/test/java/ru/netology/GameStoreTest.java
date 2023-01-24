9c1c72b0c09a64c447a6b4e80ddbf27d2c3b4de3
MillaDom <milladom@gmail.com>
2023-01-24 12:22:39 +0200

    tests changed

--- src/test/java/ru/netology/GameStoreTest.java
+++ src/test/java/ru/netology/GameStoreTest.java
@@ -8,7 +8,7 @@ import org.junit.jupiter.api.Test;
 
 public class GameStoreTest {
 
-        GameStore store = new GameStore();
+    GameStore store = new GameStore();
 
     //тесты на метод containsGame
 
@@ -16,6 +16,18 @@ public class GameStoreTest {
     public void shouldAddGame() { // должен хранить игры в каталоге, даже одну
         Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
         assertTrue(store.containsGame(game));
+    }
+
+    @Test
+    public void shouldAddSeveralGame() { // должен хранить игры в каталоге
+
+        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        Game game2 = store.publishGame("Нетология Баттл Онлайн1", "Стратегия");
+        Game game3 = store.publishGame("Нетология Баттл Онлайн2", "Quest");
+        Game game4 = store.publishGame("Нетология Баттл Онлайн3", "Shooter");
+        Game game5 = store.publishGame("Нетология Баттл Онлайн4", "Аркады");
+
+        assertTrue(store.containsGame(game3));
     }
 
     @Test
@@ -27,11 +39,20 @@ public class GameStoreTest {
     }
 
     // тесты на метод publishGame
+
+    //    @Test
+//    public void shouldNotDuplicateGame(){ // не должен создавать дублирующие объекты
+//        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+//        Game game2 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+//        assertFalse(store.containsGame(game2));
+//    }
+    //из-за добавленного exception должен быть другой тест на assertThrows
     @Test
-    public void shouldNotDuplicateGame(){ // не должен создавать дублирующие объекты
-        Game game1 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        Game game2 = store.publishGame("Нетология Баттл Онлайн", "Аркады");
-        assertTrue(store.containsGame(game2));
+    public void shouldNotAddOldGame() {
+        Game game = store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        assertThrows(RuntimeException.class, () -> {
+            store.publishGame("Нетология Баттл Онлайн", "Аркады");
+        });
     }
 
     // тесты на метод addPlayTime
@@ -44,12 +65,27 @@ public class GameStoreTest {
         assertEquals("Kolya", store.getMostPlayer());
     }
 
+    //    @Test
+//    public void couldAddNegativeOrNullTime() {// добавляет отрицательные и нулевые значения
+//        store.addPlayTime("Kolya", 2);
+//        store.addPlayTime("Petya", -5);
+//        store.addPlayTime("Olga", 0);
+//        assertEquals("Kolya", store.getMostPlayer());
+//    }
+
+    //из-за добавленного exception должен быть другой тест на assertThrows
     @Test
-    public void couldAddNegativeOrNullTime() {// должен выкидывать исключение для отрицательных и нулевых значений времени
-        store.addPlayTime("Kolya", 2);
-        store.addPlayTime("Petya", -5);
-        store.addPlayTime("Olga", 0);
-        assertEquals("Kolya", store.getMostPlayer());
+    public void couldNotAddNegativeTime() {// должен выкидывать исключение для отрицательных значений времени
+        assertThrows(RuntimeException.class, () -> {
+            store.addPlayTime("Olga", -5);
+        });
+    }
+
+    @Test
+    public void couldNotAdd0Time() {// должен выкидывать исключение для нулевых значений времени
+        assertThrows(RuntimeException.class, () -> {
+            store.addPlayTime("Kolya", 0);
+        });
     }
 
     // тесты на метод getMostPlayer
@@ -79,6 +115,17 @@ public class GameStoreTest {
 
         store.addPlayTime("Petya", 3);
         store.addPlayTime("Olga", 5);
+
+        int expected = 8;
+        int actual = store.getSumPlayedTime();
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumPlayedTimeIfOne() { //должен суммировать время игроков
+
+        store.addPlayTime("Petya", 3);
+        store.addPlayTime("Petya", 5);
 
         int expected = 8;
         int actual = store.getSumPlayedTime();
